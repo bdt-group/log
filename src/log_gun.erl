cde42ae46dcce96fe19a6d8ba73fd916816994ba
Alexander Petrov <apetrov@bdt.group>
2022-07-05 16:26:09 +0300

    Implement filtering clause for proplist

--- src/log_gun.erl
+++ src/log_gun.erl
@@ -3,10 +3,18 @@
 -export([filter_supervisor_reports/2]).
 
 -spec filter_supervisor_reports(logger:log_event(), any()) -> logger:filter_return().
-filter_supervisor_reports(#{msg := {report, Report}} = Event, _) ->
+filter_supervisor_reports(#{msg := {report, Report}} = Event, _) when is_list(Report) ->
+    case lists:sort(Report) of
+        [{errorContext, shutdown_error}, {offender, _}, {reason, Reason}, {supervisor, _}] when
+                Reason == noproc orelse Reason == {shutdown, normal} ->
+            stop;
+        _ ->
+            Event
+    end;
+filter_supervisor_reports(#{msg := {report, Report}} = Event, _) when is_map(Report)->
     case Report of
         #{reason := Reason, errorContext := shutdown_error} when
-                Reason == norpoc
+                Reason == noproc
                 orelse Reason == {shutdown, normal} ->
             stop;
         _ ->
